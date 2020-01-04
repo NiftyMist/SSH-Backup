cf314cea4e017b86a9c8ae98ecdf6db5d7a08dbe
Dylan Mitchell <mdmitch92@gmail.com>
2020-01-04 08:09:15 -0500

    more initial progress.  No testing

--- backup.sh
+++ backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # must pass the touce directory absolute path when exeucting script
 DATE=`date "+%Y-%m-%d"`
-TAR="/tmp/backup-$DATE.tar.gz
+TARBALL="/tmp/backup-$DATE.tar.gz
 LOGDIR="/var/log/scripts"
 LOGFILE="/var/log/scripts/backup-script.log"
 if [ ! -d "$LOGDIR ]; then
     /bin/mkdir $LOGDIR
     /usr/bin/touch $LOGFILE
 fi
-/bin/tar czvf $TAR $1
-rsync --remove-source-files -av $TAR $2 --log-file=$LOGFILE
+echo "### $DATE ###" >> $LOGFILE
+/bin/tar czvf $TARBALL $1
+rsync --remove-source-files -av $TAR $2 backup@10.1.2.249:/zfs-offsite/home-share/ --log-file=$LOGFILE
+echo " " >> $LOGFILE
