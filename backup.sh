9a8c0a5e18f29ac0764d8e61e26fd4307b9893e2
Dylan Mitchell <mdmitch92@gmail.com>
2020-01-04 23:03:17 -0500

    added if statment for log dir/file

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# must pass the touch directory absolute path when exeucting script
+# must pass the touch directory absolute path when exeucting script as $1
 DATE=`date "+%Y-%m-%d"`
 TARBALL="/tmp/backup-$DATE.tar.gz
 LOGDIR="/var/log/scripts"
@@ -10,6 +10,6 @@ if [ ! -d "$LOGDIR ]; then
 fi
 echo "### $DATE ###" >> $LOGFILE
 /bin/tar czvf $TARBALL $1
-# must pass the destination user, ip, and absoulte path when executing script
+# must pass the destination user, ip, and absoulte path when executing script as $2
 rsync --remove-source-files -av $TAR $2 --log-file=$LOGFILE
 echo " " >> $LOGFILE
