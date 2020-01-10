f0e8daf151b04e9f09ef08fa0f78e88360d6e4f3
Dylan Mitchell <mdmitch92@gmail.com>
2020-01-09 21:47:50 -0500

    Updated spacing and comments

--- backup.sh
+++ backup.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
 # must pass the touch directory absolute path when exeucting script as $1
+
+# Vars
 DATE=`date "+%Y-%m-%d"`
+
 TARBALL="/tmp/backup-$DATE.tar.gz
+
 LOGDIR="/var/log/scripts"
+
 LOGFILE="/var/log/scripts/backup-script.log"
+# End Vars
+
 if [ ! -d "$LOGDIR ]; then
     /bin/mkdir $LOGDIR
     /usr/bin/touch $LOGFILE
 fi
+
 echo "### $DATE ###" >> $LOGFILE
+
 /bin/tar czvf $TARBALL $1
+
 # must pass the destination user, ip, and absoulte path when executing script as $2
+
 rsync --remove-source-files -av $TAR $2 --log-file=$LOGFILE
 echo " " >> $LOGFILE
 echo " ############" >> $LOGFILE
