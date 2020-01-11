cd35abc821635cbb0306bfc85a66f611b9db4727
Dylan Mitchell <mdmitch92@gmail.com>
2020-01-11 13:57:20 -0500

    Increased the logging of the script

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 # must pass the touch directory absolute path when exeucting script as $1
 
-# Vars
+START=`date +s`
 
 DATE=`date "+%Y-%m-%d"`
 
@@ -12,8 +12,6 @@ LOGDIR="/var/log/scripts"
 
 LOGFILE="/var/log/scripts/backup-script.log"
 
-# End Vars
-
 if [ ! -d "$LOGDIR" ] 
 then
     /bin/mkdir "$LOGDIR"
@@ -21,12 +19,22 @@ then
 fi
 
 echo "$DATE" >> "$LOGFILE"
-
+LOGTIME=`date "+%Y-%m-%d %H:%M"`
+echo "$LOGTIME - Creating the tarball"
 /bin/tar czf "$TARBALL" "$1" 2> "$LOGFILE"
 
 # must pass the destination user, ip, and absoulte path when executing script as $2
 
+LOGTIME=`date "+%Y-%m-%d %H:%M"`
+echo "$LOGTIME - Starting the RSYNC"
 echo "rsync --remove-source-files -av $TAR $2 --log-file=$LOGFILE"
+echo " " >> "$LOGFILE"
+
+END=`date +s`
+
+RUNTIME=$((END-START))
+MINUTES=$((RUNTIME / 60))
+echo "Script completed in $MINUTES minutes." >> "$LOGFILE"
 echo " " >> "$LOGFILE"
 echo " ############ " >> "$LOGFILE"
 echo " " >> $LOGFILE
