7978cce536925bf1e343fd6b4e1af83eb4490881
Dylan Mitchell <mdmitch92@gmail.com>
2020-01-14 17:00:36 -0500

    corrected typos and added disk check logic

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 # must pass the source directory's absolute path when exeucting script as $1
 
-START=`date +s`
+START=`date +%s`
 
 DATE=`date "+%Y-%m-%d"`
 
@@ -18,9 +18,19 @@ then
     /usr/bin/touch "$LOGFILE"
 fi
 
-echo "$DATE" >> "$LOGFILE"
+#check remote disk space
+REMOTE_DISK_SPACE=`ssh root@bak01.niftymist.us df -Ph /zfs-bakup/offsite | tail -n 1 | awk '{ print $5 }'`
 LOGTIME=`date "+%Y-%m-%d %H:%M"`
-echo "$LOGTIME - Creating the tarball for $1 at $TARBALL" > "$LOGFILE"
+if [ "{$REMOTE_DISK_SPACE::01}" -gt 80 ]
+then 
+    echo "$LOGTIME - remote disk space check passed with $REMOTE_DISK_SPACE" >> "$LOGFILE"
+else
+    echo "$LOGIMTE - remote disk space check failed with $REMOTE_DISK_SPACE, stopping backup" >> "$LOGFILE"
+    exit 1
+fi
+
+LOGTIME=`date "+%Y-%m-%d %H:%M"`
+echo "$LOGTIME - Creating the tarball for $1 at $TARBALL" >> "$LOGFILE"
 /bin/tar czf "$TARBALL" --absolute-names "$1" 2> "$LOGFILE"
 
 # must pass the destination user, ip, and absoulte path when executing script as $2
@@ -29,7 +39,7 @@ LOGTIME=`date "+%Y-%m-%d %H:%M"`
 echo "$LOGTIME - Starting the RSYNC to $2" > "$LOGFILE"
 /usr/bin/rsync --remove-source-files -av $TARBALL $2 --log-file=$LOGFILE
 echo " " >> "$LOGFILE"
-END=`date +s`
+END=`date +%s`
 RUNTIME=$((END-START))
 MINUTES=$((RUNTIME / 60))
 LOGTIME=`date "+%Y-%m-%d %H:%M"`
