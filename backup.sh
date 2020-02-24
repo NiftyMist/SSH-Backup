8589a486af685158d098d2944dd1b01ec01b3f7c
Dylan Mitchell <mdmitch92@gmail.com>
2020-02-24 10:28:25 -0500

    changed from pgunzip to pbzip2 compression

--- backup.sh
+++ backup.sh
@@ -16,7 +16,7 @@ START=`date +%s`
 
 DATE=`date "+%Y-%m-%d"`
 
-TARBALL="/tmp/backup-$DATE.tar.gz"
+TARBALL="/tmp/backup-$DATE.tbz"
 
 LOGDIR="/var/log/scripts"
 
@@ -54,7 +54,7 @@ fi
 
 LOGTIME=`date "+%Y/%m/%d %H:%M:%S"`
 echo "$LOGTIME - creating the tarball for $SOURCE at $TARBALL" >> "$LOGFILE"
-/bin/tar -I pigz -cf "$TARBALL" --absolute-names "$SOURCE" 2>> "$LOGFILE"
+/bin/tar -I pbzip2 -cf "$TARBALL" --absolute-names "$SOURCE" 2>> "$LOGFILE"
 
 LOGTIME=`date "+%Y/%m/%d %H:%M:%S"`
 echo "$LOGTIME - starting the RSYNC to $REMOTE_HOST" >> "$LOGFILE"
